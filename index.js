700ee03786cbebc5106219be275eb3cb1323974a
Sarah Lindmar <srhcrete@gmail.com>
2018-02-11 20:37:38 -0800

    add card-section and button components

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 
-const App = () => {
-  return (
+const App = () => (
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
   );
-};
 
 
 // Render it to the device
